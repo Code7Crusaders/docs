fa295b6edf343618f1cf3af30b9b1da637530128
Enrico Cotti Cottini <enrico.cotticottini@gmail.com>
2024-10-16 19:52:48 +0200

    fatta bozza di template creato script per pulire

--- clean.sh
+++ clean.sh
@@ -10,7 +10,7 @@ initial_path=$(pwd)
 
 for file in ${files[@]}; do
     if cd $(dirname $file); then
-    rm -f $(basename $file .tex).log $(basename $file .tex).aux $(basename $file .tex).out $(basename $file .tex).toc 
+    rm -f $(basename $file .tex).log $(basename $file .tex).aux $(basename $file .tex).out $(basename $file .tex).toc
     fi
     cd $initial_path
 done
