5e23ca7ce2766aa812d06f970ed4e4551e116280
Enrico Cotti Cottini <enrico.cotticottini@gmail.com>
2024-10-16 16:52:49 +0200

    aggiunto script per compilare .tex, e file template e domande

---
+++ compile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+files=(
+    "./Candidatura/DomandeProponenti/Domande_Proponenti.tex"
+)
+
+for file in ${files[@]}; do
+  if cd $(dirname $file); then
+    pdflatex -interaction=batchmode $(basename $file)
+    echo "File $(basename $file .tex).pdf created"
+    cd -
+  else
+    echo "Failed to change directory to $(dirname $file)"
+  fi
+done
+
+for file in ${files[@]}; do
+    if cd $(dirname $file); then
+    rm $(basename $file .tex).log $(basename $file .tex).aux
+    fi
+done
+
