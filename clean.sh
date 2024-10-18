4b48982dab423b1426abe5b2f6f3095969f88cd6
Enrico Cotti Cottini <enrico.cotticottini@gmail.com>
2024-10-18 14:41:20 +0200

    Creato verbale ergon

--- clean.sh
+++ clean.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 # Per cancellare file ausiliari
 files=(
+    "./Candidatura/Lettera_di_Presentazione.tex"
     "./Candidatura/DomandeProponenti/Domande_Proponenti.tex"
+    "./Candidatura/verbali_interni/verbale_171024_azzurrodigitaleint_v0.1.tex"
+    "./Candidatura/verbali_esterni/verbale_181024_ergon.tex"
+    "./Candidatura/verbali_esterni/verbale_171024_azzurodigitale_v0.1.tex"
 )
 
 
@@ -9,7 +13,7 @@ initial_path=$(pwd)
 
 for file in ${files[@]}; do
     if cd $(dirname $file); then
-    rm -f $(basename $file .tex).log $(basename $file .tex).aux $(basename $file .tex).out $(basename $file .tex).toc
+    rm -f $(basename $file .tex).log $(basename $file .tex).aux $(basename $file .tex).out $(basename $file .tex).toc $(basename $file .tex).pdf
     fi
     cd $initial_path
 done
