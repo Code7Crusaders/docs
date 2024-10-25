d2daf92640203d4cff265b613b58890ce6e0cd70
Enrico Cotti Cottini <enrico.cotticottini@gmail.com>
2024-10-25 16:16:06 +0200

    valutazione capit aggiornamento

--- clean.sh
+++ clean.sh
@@ -4,6 +4,7 @@ files=(
     "./Candidatura/DomandeProponenti/Domande_Proponenti.tex"
     "./Candidatura/Lettera_di_Presentazione.tex"
     "./Candidatura/Valutazione_Capitolati.tex"
+    "./Candidatura/analisi_costi_assunzione_impegni.tex"
     "./Candidatura/verbali_esterni/verbale_181024_ergon_v1.0.tex"
     "./Candidatura/verbali_esterni/verbale_171024_azzurodigitale_v1.0.tex"
     "./Candidatura/verbali_esterni/verbale_181024_sanmarco_v1.0.tex"
