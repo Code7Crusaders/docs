507210d0a298b1bfc059ae30be236fdf7d5ca197
Enrico Cotti Cottini <enrico.cotticottini@gmail.com>
2024-10-25 21:59:41 +0200

    Sistemazione dile system, cancellati doppioni verbali ext/int

--- clean.sh
+++ clean.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 # Per cancellare file ausiliari
 files=(
+
     "./Candidatura/DomandeProponenti/Domande_Proponenti.tex"
+    
     "./Candidatura/Lettera_di_Presentazione.tex"
     "./Candidatura/Valutazione_Capitolati.tex"
     "./Candidatura/analisi_costi_assunzione_impegni.tex"
-    "./Candidatura/verbali_esterni/verbale_181024_ergon_v1.0.tex"
-    "./Candidatura/verbali_esterni/verbale_171024_azzurodigitale_v1.0.tex"
-    "./Candidatura/verbali_esterni/verbale_181024_sanmarco_v1.0.tex"
-    "./Candidatura/verbali_interni/verbale_15182024_v1.0.tex"
-    "./Candidatura/verbali_interni/verbale_18102024_v1.0.tex"
 
     "./Candidatura/verbali_esterni/verbale_24-10-18_ergon.tex"
-    "./Candidatura/verbali_esterni/verbale_24-10-17_azzurodigitale_v0.1.tex"
     "./Candidatura/verbali_esterni/verbale_24-10-17_azzurodigitale.tex"
     "./Candidatura/verbali_esterni/verbale_24-10-18_sanmarco.tex"
+
     "./Candidatura/verbali_interni/verbale_24-10-18_v1.0.tex"
     "./Candidatura/verbali_interni/verbale_24-10-15_v1.0.tex"
     "./Candidatura/verbali_interni/verbale_24-10-25_v0.1.tex"
 
     "./templateC7C/template.tex"
     "./templateC7C/slide.tex"
+
 )
 
 
