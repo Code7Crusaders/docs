90e33aed74876d318cec6afdd5a3a49c4a86d994
Filippo <rizzolofilippo18@gmail.com>
2024-10-29 09:21:56 +0100

    Fixato problema stile closes #15 e revisione finale del verbale interno 24-10-25 closes #12

--- clean.sh
+++ clean.sh
@@ -2,24 +2,24 @@
 # Per cancellare file ausiliari
 files=(
 
-    "./Candidatura/DomandeProponenti/Domande_Proponenti.tex"
+    # "./Candidatura/DomandeProponenti/Domande_Proponenti.tex"
     
-    "./Candidatura/Lettera_di_Presentazione.tex"
-    "./Candidatura/Valutazione_Capitolati.tex"
-    "./Candidatura/analisi_costi_assunzione_impegni.tex"
+    # "./Candidatura/Lettera_di_Presentazione.tex"
+    # "./Candidatura/Valutazione_Capitolati.tex"
+    # "./Candidatura/analisi_costi_assunzione_impegni.tex"
 
-    "./Candidatura/verbali_esterni/verbale_24-10-18_ergon.tex"
-    "./Candidatura/verbali_esterni/verbale_24-10-17_azzurodigitale.tex"
-    "./Candidatura/verbali_esterni/verbale_24-10-18_sanmarco.tex"
+    # "./Candidatura/verbali_esterni/verbale_24-10-18_ergon.tex"
+    # "./Candidatura/verbali_esterni/verbale_24-10-17_azzurodigitale.tex"
+    # "./Candidatura/verbali_esterni/verbale_24-10-18_sanmarco.tex"
 
-    "./Candidatura/verbali_interni/verbale_24-10-18_v1.0.tex"
-    "./Candidatura/verbali_interni/verbale_24-10-15_v1.0.tex"
-    "./Candidatura/verbali_interni/verbale_24-10-25_v0.1.tex"
+    # "./Candidatura/verbali_interni/verbale_24-10-18_v1.0.tex"
+    # "./Candidatura/verbali_interni/verbale_24-10-15_v1.0.tex"
+    # "./Candidatura/verbali_interni/verbale_24-10-25_v1.0.tex"
 
-    "./DiariDiBordo/Diario_28-10-2024.tex"
+    # "./DiariDiBordo/Diario_28-10-2024.tex"
 
-    "./templateC7C/template.tex"
-    "./templateC7C/slide.tex"
+    # "./templateC7C/template.tex"
+    # "./templateC7C/slide.tex"
 
 )
 
