a98daf54890920720f148c13c8b81704d879c1b6
Enrico Cotti Cottini <enrico.cotticottini@gmail.com>
2024-11-27 20:25:51 +0100

    aggiunte immagini e descrizione casi uso closes #51

--- compile.sh
+++ compile.sh
@@ -22,20 +22,20 @@ files=(
     # "./src/DiariDiBordo/Diario_24-11-18.tex"
     # "./src/DiariDiBordo/Diario_24-10-28.tex"
     # "./src/DiariDiBordo/Diario_24-11-12.tex"
-    "./src/DiariDiBordo/Diario_24-11-25.tex"
+    # "./src/DiariDiBordo/Diario_24-11-25.tex"
 
     # "./src/templateC7C/template.tex"
     # "./src/templateC7C/slide.tex"
     # "./src/templateC7C/template-verbali-nuovo/template-verbali.tex"
 
 
-    # "./src/2_RTB/documentazione_interna/glossario.tex"
-    # "./src/2_RTB/documentazione_interna/norme_di_progetto.tex"
+    "./src/2_RTB/documentazione_interna/glossario.tex"
+    "./src/2_RTB/documentazione_interna/norme_di_progetto.tex"
     
-    # "./src/2_RTB/verbali_esterni/verbale_24-11-14_v0.1.tex"
+    "./src/2_RTB/verbali_esterni/verbale_24-11-14_v0.1.tex"
 
-    # "./src/2_RTB/verbali_interni/verbale_24-11-04_v0.1.tex" 
-    # "./src/2_RTB/verbali_interni/verbale_24-11-15_v0.1.tex"
+    "./src/2_RTB/verbali_interni/verbale_24-11-04_v0.1.tex" 
+    "./src/2_RTB/verbali_interni/verbale_24-11-15_v0.1.tex"
 
 )
 
