c2580ccc5da25bbcfc25702fce74be82ff756e24
Enrico Cotti Cottini <enrico.cotticottini@gmail.com>
2024-11-29 15:45:33 +0100

    inizio verbale 29 nov

--- compile.sh
+++ compile.sh
@@ -29,13 +29,14 @@ files=(
     # "./src/templateC7C/template-verbali-nuovo/template-verbali.tex"
 
 
-    "./src/2_RTB/documentazione_interna/glossario.tex"
-    "./src/2_RTB/documentazione_interna/norme_di_progetto.tex"
+    # "./src/2_RTB/documentazione_interna/glossario.tex"
+    # "./src/2_RTB/documentazione_interna/norme_di_progetto.tex"
     
-    "./src/2_RTB/verbali_esterni/verbale_24-11-14_v0.1.tex"
+    # "./src/2_RTB/verbali_esterni/verbale_24-11-14_v0.1.tex"
 
-    "./src/2_RTB/verbali_interni/verbale_24-11-04_v0.1.tex" 
-    "./src/2_RTB/verbali_interni/verbale_24-11-15_v0.1.tex"
+    # "./src/2_RTB/verbali_interni/verbale_24-11-04_v0.1.tex" 
+    # "./src/2_RTB/verbali_interni/verbale_24-11-15_v0.1.tex"
+    "./src/2_RTB/verbali_interni/verbale_24-11-29_v1.0.tex"
 
 )
 
