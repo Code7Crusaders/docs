e34ef4bd1548691e3ccc42fc756d00fcb23d0f8f
tommasodiviesti <tommaso.diviesti@studenti.unipd.it>
2024-12-03 12:05:03 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -29,7 +29,7 @@ files=(
     # "./src/templateC7C/slide.tex"
     # "./src/templateC7C/template-verbali-nuovo/template-verbali.tex"
 
-    # "./src/2_RTB/documentazione_esterna/piano_di_progetto_v0.1.tex"
+    # "./src/2_RTB/documentazione_esterna/piano_di_progetto_v0.2.tex"
     
     # "./src/2_RTB/documentazione_interna/glossario.tex"
     # "./src/2_RTB/documentazione_interna/norme_di_progetto.tex"
