f6866a94a391a098369eea4960fa748c8d21d6bc
francesco-lapenna <francesco.lapenna.d@gmail.com>
2025-01-07 13:44:24 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -44,6 +44,7 @@ files=(
     # "./src/templateC7C/template-verbali-nuovo/template-verbali.tex"
 
     "./src/2_RTB/documentazione_esterna/piano_di_progetto_v0.2.tex"
+    "./src/2_RTB/documentazione_esterna/piano_di_progetto_v0.3.tex"
      "./src/2_RTB/documentazione_esterna/piano_di_qualifica_v0.1.tex"
      "./src/2_RTB/documentazione_esterna/analisi_dei_requisiti/analisi_dei_requisiti.tex"
     
