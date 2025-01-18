293acb88700a598bc3428233b4cac3f5e2f2f665
tommasodiviesti <tommaso.diviesti@studenti.unipd.it>
2025-01-18 18:56:12 +0100

    aggiornamento rischi

--- compile.sh
+++ compile.sh
@@ -45,7 +45,7 @@ files=(
     # "./src/templateC7C/slide.tex"
     # "./src/templateC7C/template-verbali-nuovo/template-verbali.tex"
 
-    "./src/2_RTB/documentazione_esterna/piano_di_progetto_v0.4.tex"
+    "./src/2_RTB/documentazione_esterna/piano_di_progetto_v0.5.tex"
      "./src/2_RTB/documentazione_esterna/piano_di_qualifica_v0.3.tex"
      "./src/2_RTB/documentazione_esterna/analisi_dei_requisiti/analisi_dei_requisiti.tex"
     
