9c65595a8a0e5e10da3edc04a0069a822e50f499
SerpenTaki <gabrielecorubolo@gmail.com>
2025-02-06 17:21:13 +0100

    aggiunti testi di accettazione

--- compile.sh
+++ compile.sh
@@ -45,8 +45,6 @@ files=(
     # "./src/templateC7C/slide.tex"
     # "./src/templateC7C/template-verbali-nuovo/template-verbali.tex"
 
-    "./src/2_RTB/documentazione_esterna/piano_di_progetto_v0.5.tex"
-    "./src/2_RTB/documentazione_esterna/piano_di_progetto_v0.6.tex"
     "./src/2_RTB/documentazione_esterna/piano_di_progetto_v0.7.tex"
     "./src/2_RTB/documentazione_esterna/piano_di_qualifica_v0.3.tex"
     "./src/2_RTB/documentazione_esterna/analisi_dei_requisiti/analisi_dei_requisiti.tex"
