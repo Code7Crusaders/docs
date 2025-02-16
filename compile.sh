ab15a667cc276de4c33ea37ddf2bd11921c2c55e
Enrico Cotti Cottini <enrico.cotticottini@gmail.com>
2025-02-16 17:17:38 +0100

    agggiunta documento di analisi del database vettoriale #142

--- compile.sh
+++ compile.sh
@@ -68,12 +68,13 @@ files=(
     # "./src/2_RTB/verbali_interni/verbale_25-01-18_v1.0.tex"
     # "./src/2_RTB/verbali_interni/verbale_25-01-24_v1.0.tex"
     # "./src/2_RTB/verbali_interni/verbale_25-02-04_v1.0.tex"
-     "./src/2_RTB/verbali_interni/verbale_25-02-07_v1.0.tex"
+    #  "./src/2_RTB/verbali_interni/verbale_25-02-07_v1.0.tex"
 
     # "src/altri_documenti/analisi_LLM_Bloom_Openai_gpt4/analisi_modelli.tex"
     # "src/altri_documenti/analisi_framework_frontend/analisi_frontend.tex"
     # "src/altri_documenti/presentazione_rtb/presentazione_rtb.tex"
-     "src/altri_documenti/analisi_framework_backend/analisi_backend.tex"
+    #  "src/altri_documenti/analisi_framework_backend/analisi_backend.tex"
+     "src/altri_documenti/analisi_dbvettoriale/analisi_vettoriale.tex"
 
     #  "./src/2_RTB/lettera_di_presentazione.tex"
 
