79bf02e49e231717a25d5c74bd2bb4644aafd09d
Filippo <rizzolofilippo18@gmail.com>
2025-03-11 16:54:58 +0100

    Verificato verbale 7 marzo

--- compile.sh
+++ compile.sh
@@ -82,18 +82,18 @@ files=(
 
     # PB
 
-     "./src/3_PB/documentazione_esterna/analisi_dei_requisiti/analisi_dei_requisiti_v3.0.tex"
-     "./src/3_PB/documentazione_esterna/piano_di_progetto/piano_di_progetto_v2.0.tex"
-     "./src/3_PB/documentazione_esterna/piano_di_qualifica/piano_di_qualifica_v2.0.tex"
-     "./src/3_PB/documentazione_esterna/manuale_utente/manuale_utente_v1.0.tex"
-     "./src/3_PB/documentazione_esterna/specifica_tecnica/specifica_tecnica_v1.0.tex"
+    #  "./src/3_PB/documentazione_esterna/analisi_dei_requisiti/analisi_dei_requisiti_v3.0.tex"
+    #  "./src/3_PB/documentazione_esterna/piano_di_progetto/piano_di_progetto_v2.0.tex"
+    #  "./src/3_PB/documentazione_esterna/piano_di_qualifica/piano_di_qualifica_v2.0.tex"
+    #  "./src/3_PB/documentazione_esterna/manuale_utente/manuale_utente_v1.0.tex"
+    #  "./src/3_PB/documentazione_esterna/specifica_tecnica/specifica_tecnica_v1.0.tex"
 
-     "./src/3_PB/documentazione_interna/glossario/glossario_v2.0.tex"
-     "./src/3_PB/documentazione_interna/norme_di_progetto/norme_di_progetto_v2.0.tex"
+    #  "./src/3_PB/documentazione_interna/glossario/glossario_v2.0.tex"
+    #  "./src/3_PB/documentazione_interna/norme_di_progetto/norme_di_progetto_v2.0.tex"
 
-    "./src/3_PB/verbali_esterni/verbale_25-03-04_v1.0.tex"
+    # "./src/3_PB/verbali_esterni/verbale_25-03-04_v1.0.tex"
 
-    "./src/3_PB/verbali_interni/verbale_25-02-28_v1.0.tex"
+    # "./src/3_PB/verbali_interni/verbale_25-02-28_v1.0.tex"
     "./src/3_PB/verbali_interni/verbale_25-03-07_v1.0.tex"
 
     # "./src/3_PB/lettera_di_presentazione/..."
