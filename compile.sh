9311a87c1334a94ed4d0afcd6073ae6d82a1c1f0
SerpenTaki <gabrielecorubolo@gmail.com>
2025-03-25 08:56:17 +0100

    verifica verbale e generazione pdf

--- compile.sh
+++ compile.sh
@@ -96,6 +96,7 @@ files=(
 
     # "./src/3_PB/verbali_interni/verbale_25-02-28_v1.0.tex"
     # "./src/3_PB/verbali_interni/verbale_25-03-07_v1.0.tex"
+    "./src/3_PB/verbali_interni/verbale_25-03-14_v1.0.tex"
 
     # "./src/3_PB/lettera_di_presentazione/..."
 )
