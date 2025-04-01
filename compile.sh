d44115ef63ee24775aa706d20d6ee242717cac0a
SerpenTaki <gabrielecorubolo@gmail.com>
2025-04-02 00:36:39 +0200

    aggiornato manuale utente

--- compile.sh
+++ compile.sh
@@ -85,19 +85,19 @@ files=(
 
     # PB
 
-        # "./src/3_PB/documentazione_esterna/analisi_dei_requisiti/analisi_dei_requisiti_v3.0.tex"
-        # "./src/3_PB/documentazione_esterna/piano_di_progetto/piano_di_progetto_v2.0.tex"
-        # "./src/3_PB/documentazione_esterna/piano_di_qualifica/piano_di_qualifica_v2.0.tex"
-        # "./src/3_PB/documentazione_esterna/manuale_utente/manuale_utente_v1.0.tex"
-        # "./src/3_PB/documentazione_esterna/specifica_tecnica/specifica_tecnica_v1.0.tex"
+         "./src/3_PB/documentazione_esterna/analisi_dei_requisiti/analisi_dei_requisiti_v3.0.tex"
+         "./src/3_PB/documentazione_esterna/piano_di_progetto/piano_di_progetto_v2.0.tex"
+         "./src/3_PB/documentazione_esterna/piano_di_qualifica/piano_di_qualifica_v2.0.tex"
+         "./src/3_PB/documentazione_esterna/manuale_utente/manuale_utente_v1.0.tex"
+         "./src/3_PB/documentazione_esterna/specifica_tecnica/specifica_tecnica_v1.0.tex"
         "./src/3_PB/documentazione_esterna/manuale_utente/manuale_utente_v1.0.tex"
         # "./src/3_PB/documentazione_esterna/manuale_utente/introduzione.tex"
         # "./src/3_PB/documentazione_esterna/manuale_utente/requisiti.tex"
         # "./src/3_PB/documentazione_esterna/manuale_utente/supporto.tex"
-        "./src/3_PB/documentazione_esterna/manuale_utente/istruzioni.tex"
+        #"./src/3_PB/documentazione_esterna/manuale_utente/istruzioni.tex"
 
-        # "./src/3_PB/documentazione_interna/glossario/glossario_v2.0.tex"
-        # "./src/3_PB/documentazione_interna/norme_di_progetto/norme_di_progetto_v2.0.tex"
+         "./src/3_PB/documentazione_interna/glossario/glossario_v2.0.tex"
+         "./src/3_PB/documentazione_interna/norme_di_progetto/norme_di_progetto_v2.0.tex"
 
     # "./src/3_PB/verbali_esterni/verbale_25-03-04_v1.0.tex"
     #  "./src/3_PB/verbali_esterni/verbale_25-03-13_v1.0.tex"
